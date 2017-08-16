19d3d2eedef97ef8fb3d0b45ec347c1a48550d4d
yinm <yinm.cminor9@gmail.com>
2017-08-16 18:35:30 +0900

    add valid and invalid validation check

--- moment-js/valid.js
+++ moment-js/valid.js
@@ -3,3 +3,8 @@ const moment = require('moment');
 const m = moment('2011-10-10T10:20:90');
 console.log(m.isValid());
 console.log(m.invalidAt());
+
+const validMoment = moment('2017-08-16');
+console.log(validMoment.isValid());
+
+console.log(moment('test').isValid());
