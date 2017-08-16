47e225a949e35d9873b1deb4a41d27856386d1f1
yinm <yinm.cminor9@gmail.com>
2017-08-16 15:40:21 +0900

    date validation check

--- moment-js/app.js
+++ moment-js/app.js
@@ -1,2 +1,18 @@
 const moment = require('moment');
-console.log(moment('2017-08-16T05:57:32Z').format());
+
+const before = moment('2017-08-16');
+const same = moment('2017-08-16');
+const designation = moment('2017-08-16T05:57:32Z');
+const after = moment('2017-08-17');
+
+if (designation.isBefore(after)) {
+  console.log('before');
+} else {
+  console.log('after');
+}
+
+if (designation.isAfter(same)) {
+  console.log('after');
+} else {
+  console.log('before');
+}
